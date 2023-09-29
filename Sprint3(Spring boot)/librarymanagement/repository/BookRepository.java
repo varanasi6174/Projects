ca63a2ea69556d1c9a6f0e5b1cc6b0d690553b4d
Ravi Pal <137257702+varanasi6174@users.noreply.github.com>
2023-09-29 14:22:29 +0530

    Delete Sprint3(Spring boot) directory

--- Sprint3(Spring boot)/librarymanagement/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.librarymanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.librarymanagement.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-
-}
--- Sprint3(Spring boot)/librarymanagement/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.librarymanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.librarymanagement.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
